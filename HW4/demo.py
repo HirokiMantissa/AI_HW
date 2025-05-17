6f3459c63f6c770032ee2ad6e5e0416193463f1a
HirokiMantissa <131464134+HirokiMantissa@users.noreply.github.com>
2025-05-17 17:51:29 +0800

    Add files via upload

--- HW4/demo.py
+++ HW4/demo.py
@@ -2,70 +2,59 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 
+from V import value_iteration, plot_policy_map, plot_value_map
+
 class Game:
     def __init__(self):
         self.bomb = "bomb"
         self.wall = "wall"
         self.final = "final"
 
-        self.gamma = 0.9
-        self.start = (2, 0)
-        self.end = (0, 3)
-        self.current_state = self.start
-
         self.map = np.array([
             [0, 0, 0, self.final],
             [0, self.wall, 0, self.bomb],
             [0, 0, 0, 0]
         ])
-    
+
+class Agent:
+    def __init__(self, env: Game):
+        self.env = env
+        self.actions = ["up", "down", "left", "right"]
+        
+        self.gamma = 0.9
+        self.start = (2, 0)
+        self.end = (0, 3)
+        self.current_state = self.start
+        
     def get_next_state(self, state, action):
         i, j = state
         if action == "up":
             i = max(i - 1, 0)
         elif action == "down":
-            i = min(i + 1, self.map.shape[0] - 1)
+            i = min(i + 1, self.env.map.shape[0] - 1)
         elif action == "left":
             j = max(j - 1, 0)
         elif action == "right":
-            j = min(j + 1, self.map.shape[1] - 1)
-    
-    
-        if self.map[i, j] == self.wall:
+            j = min(j + 1, self.env.map.shape[1] - 1)
+
+        if self.env.map[i, j] == self.env.wall:
             return state
         return (i, j)
-    
+
     def get_reward(self, state):
-        val = self.map[state]
-        if val == self.final:
+        cell = self.env.map[state]
+        if cell == self.env.final:
             return 10
-        elif val == self.bomb:
+        elif cell == self.env.bomb:
             return -10
-        elif val == self.wall:
+        elif cell == self.env.wall:
             return 0
         else:
-            return -1
+            return -1 
 
     def is_terminal(self, state):
-        return self.map[state] in [self.final, self.bomb]
+        return self.env.map[state] in [self.env.final, self.env.bomb]
 
-    def reset(self):
-        self.current_state = self.start
-        return self.current_state
-
-class Agent:
-    def __init__(self, env: Game):
-        self.env = env
-        self.state = env.reset()
-        self.actions = ["up", "down", "left", "right"]
-        self.gamma = env.gamma
-    
-    def act(self, action):
-        next_state = self.env.get_next_state(self.state, action)
-        reward = self.env.get_reward(next_state)
-        done = self.env.is_terminal(next_state)
-        self.state = next_state
-        return next_state, reward, done
 
 
 def plot_map(game: Game, agent_pos=None):
@@ -113,4 +102,15 @@ def plot_map(game: Game, agent_pos=None):
     plt.show()
     
 game = Game()
-plot_map(game, agent_pos=game.current_state)
+agent = Agent(game)
+V, policy = value_iteration(agent, max_iter=10)
+plot_value_map(game, V, agent_pos=agent.current_state)
+plot_policy_map(game, policy, agent_pos=agent.current_state)
+
+
+
+print("State Value V*(s):")
+print(np.round(V, 2))
+
+print("\nOptimal Policy π*(s):")
+print(policy)
